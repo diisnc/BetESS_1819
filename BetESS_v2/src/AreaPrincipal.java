7a88b83f78694407694a82b282bb18cdb7efa4b7
Marco Silva <marant_silva@hotmail.com>
2018-11-29 13:48:35 +0000

    forms for UI

--- BetESS_v2/src/AreaPrincipal.java
+++ BetESS_v2/src/AreaPrincipal.java
@@ -4,16 +4,12 @@
  * and open the template in the editor.
  */
 
-/**
- *
- * @author MarcoSilva
- */
-public class View extends javax.swing.JFrame {
+public class AreaPrincipal extends javax.swing.JFrame {
 
     /**
-     * Creates new form View
+     * Creates new form AreaAdministrador
      */
-    public View() {
+    public AreaPrincipal() {
         initComponents();
     }
 
@@ -59,20 +55,21 @@ public class View extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(View.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AreaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(View.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AreaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(View.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AreaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(View.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AreaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new View().setVisible(true);
+                new AreaPrincipal().setVisible(true);
             }
         });
     }
