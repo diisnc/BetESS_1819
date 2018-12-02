c6d9aec49d658ed38f8f15acf6831e202df3d14b
Marco Silva <marant_silva@hotmail.com>
2018-12-02 12:47:48 +0000

    responsive UI to type of user logged in implemented (other functionalities not tested yet)

--- BetESS_v2/src/AreaUI.java
+++ BetESS_v2/src/AreaUI.java
@@ -16,8 +16,20 @@ public class AreaUI extends javax.swing.JFrame implements Observer{
     /**
      * Creates new form AreaUI
      */
-    public AreaUI() {
+    public AreaUI(String email) {
         initComponents();
+        System.out.println(email);
+        
+        /* remoção de paineis anteriores */
+        area_administrador.setVisible(false);
+        area_cliente.setVisible(false);
+        
+        if (email.equals("admin")){
+            area_administrador.setVisible(true);
+        }
+        else {
+            area_cliente.setVisible(true);
+        }
     }
 
     public void setMymodel(Model mymodel) {
@@ -1988,13 +2000,13 @@ public class AreaUI extends javax.swing.JFrame implements Observer{
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    //public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
-        try {
+        /*try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
@@ -2009,16 +2021,16 @@ public class AreaUI extends javax.swing.JFrame implements Observer{
             java.util.logging.Logger.getLogger(AreaUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(AreaUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
+        }*/
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
+        /*java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new AreaUI().setVisible(true);
             }
         });
-    }
+    }*/
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel Welcome_label_admin;
